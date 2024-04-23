d0595bbbda535f072a5a412269e3e803c01ef791
EspenB08 <142892271+EspenB08@users.noreply.github.com>
2024-04-23 10:31:28 +0200

    finished boss list

---
+++ bingoProject/src/components/randomBoss.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import runescapeBosses from "../assets/BossList";
+
+function randomBoss(){
+  const [RandomizedBoss, setRandomizedBoss] = useState(null);
+
+  const handleRandomizedBoss = (event) => {
+    setSelectedBoss(event.target.value);
+}
+return(
+    <div>
+        
+    </div>
+)
+}
+export default randomBoss
